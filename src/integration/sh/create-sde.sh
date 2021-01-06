3d5de8ddac8dccea13de57c99f088f8c575b997f
Adam Antinoo <adamantinoo.git@gmail.com>
2021-01-06 16:28:58 +0100

    -[INTEGRATION] Upgrade the SDE database generation process.

--- src/integration/sh/create-sde.sh
+++ src/integration/sh/create-sde.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 figlet CreateSDE
 echo "> Database creation started."
+mv ./downloads/*.csv .
 
-#echo ">> Remove current sde database..."
-#rm sde.db
+echo ">> Remove current sde database..."
+rm sde.db
 echo ">> Create new sde database..."
 /usr/bin/sqlite3 sde.db < db-ddl-0.20.0.ddl
 cp sde.db ../resources/
 echo "> Database creation completed."
+rm -rf *.csv
--- src/integration/sh/install-sde.sh
+++ src/integration/sh/install-sde.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 figlet InstallSDE
+./update-sde.sh
 ./create-sde.sh
 
 echo "> Moving database to destinations."
 mv sde.db ../resources/
-rm -rf *.csv
 echo "> SDE db installation completed."
